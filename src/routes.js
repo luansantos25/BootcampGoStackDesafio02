c3daa80fdd34c2033f97052d30f1d31c42a00203
Luan <luan.dev.santos@gmail.com>
2020-02-07 23:59:24 -0200

    initial sequelize settings and first create user test

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
-import {Router} from 'express';
+import { Router } from 'express';
+
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({message: 'Hello World'});
+routes.get('/', async (req, res) => {
+  await User.create({
+    email: 'luan@test.com',
+    password_hash: '123456',
+  });
+  res.json({ message: 'Hello World' });
 });
-
 export default routes;
