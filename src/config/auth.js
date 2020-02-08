5c2c5a76e7bab3c81f0f851005288be9e674212f
Luan <luan.dev.santos@gmail.com>
2020-02-08 20:50:10 -0200

    add session validator with jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ad5e48a91de6da6efc7ae33d5d798479',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddlewares);
+
+routes.put('/users', (req, res) => {
+  return res.json({ test: 'todo' });
+});
 
 export default routes;
